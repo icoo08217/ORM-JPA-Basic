76fa6a577a4d5116696e6426bf3b592458822d7b
byungchan <icoo08216@gmail.com>
2022-03-23 20:38:13 +0900

    22.03.23

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,9 @@ package hellojpa;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
@@ -19,35 +22,27 @@ public class JpaMain {
 
         try { // 정석 코드
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity" , "street" , "10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1" , "street" , "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2" , "street" , "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("============ START ===============");
-            Member findMember = em.find(Member.class, member.getId());
-
-            //homeCity => newCity
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity" , a.getStreet() , a.getZipcode()));
+//            // JPQL --> 객체지향 SQL
+//            String qlString = "select m From Member as m where m.username like '%kim%'"; // 문자 형식이기때문에 동적 쿼리를 해결하기 너무 어렵다.
+//            List<Member> resultList = em.createQuery(
+//                    qlString,
+//                    Member.class
+//            ).getResultList();
 //
-//            // 치킨 -> 파전
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("파전");
-//
-//            findMember.getAddressHistory().remove(new Address("old1" , "street" , "10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1" , "street" , "10000"));
+//            for (Member member : resultList) {
+//                System.out.println("member = " + member);
+//            }
+
+            //Criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+            Root<Member> m = query.from(Member.class);
+
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+            List<Member> resultList = em.createQuery(cq).getResultList();
+
+            // flush -> commit , query 전에 항상 실행됨.
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,7 +1,6 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
